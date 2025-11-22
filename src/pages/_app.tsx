40e97e792ea2f4567100155b2bea877159e4000f
crististg <cristi.stiegelbauer@gmail.com>
2025-11-22 18:33:14 +0200

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
