cb700f95ced57ef925cfc85380b7f06d84e8f2ae
crististg <cristi.stiegelbauer@gmail.com>
2025-11-22 18:49:38 +0200

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,5 +6,6 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return <SessionProvider session={session}>
     <Component {...pageProps} />
+    <SpeedInsights />
   </SessionProvider>;
 }
