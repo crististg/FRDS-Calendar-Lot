217e17913ed43bb29b76cd25a1a537fa2982d9c9
crististg <cristi.stiegelbauer@gmail.com>
2025-11-22 18:20:37 +0200

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,28 @@
+import { GetServerSideProps } from 'next'
+import { getServerSession } from 'next-auth/next'
+import { authOptions } from './api/auth/[...nextauth]'
+
 export default function HomePage() {
   return (
-    <main>
-    </main>
+    <main />
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getServerSession(context.req, context.res, authOptions as any)
+  if (session) {
+    return {
+      redirect: {
+        destination: '/app',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    redirect: {
+      destination: '/login',
+      permanent: false,
+    },
+  }
 }
